2f5926e8fdfa2501164908270d176d2e1bf4be00
Huelva91 <huelva91gmail.com>
2022-05-19 18:03:08 +0200

    ADD contador

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import "./App.css";
 import fccLogo from "./imagenes/freecodecamp-logo.png";
+import Boton from "./componentes/Boton";
+import Contador from "./componentes/Contador";
 
 function App() {
+  const manejarClick = () => {
+    console.log("Click!");
+  };
+  const reiniciarContador = () => {
+    console.log("Reiniciar!");
+  };
+
   return (
     <div className="App">
       <div className="fcc-logo-contenedor">
-        <img
-          src={fccLogo}
-          alt="Free Code Camp Logo"
-          className="fcc-logo"
-        />
+        <img src={fccLogo} alt="Free Code Camp Logo" className="fcc-logo" />
       </div>
 
       <div className="contenedor-principal">
-        
+        <Contador numclicks={5} />
+        <Boton
+          texto="click"
+          esBotonDeClick={true}
+          manejarClick={manejarClick}
+        />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClick={false}
+          manejarClick={reiniciarContador}
+        />
       </div>
     </div>
   );
---
+++ src/componentes/Contador.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Contador({ numclicks }) {
+  return(
+       <div className="contador">
+           {numclicks}
+       </div>
+       );
+}
+
+export default Contador;
